24d075db6219dedd42c68539bfb6868a2c4c3d53
Derick Del Rosario <derickmitchel@icloud.com>
2020-06-17 09:45:46 -0700

    nav and divs with content

---
+++ src/components/HomeBanner.jsx
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+class HomeBanner extends Component {
+    render() { 
+        return (
+            <nav class=''>
+                <div class=''>
+                    <ul class=''>
+                        <li>About</li>
+                        <li>Gaming</li>
+                        <li>Art</li>
+                        <li>Kendama</li>
+                        <li>Projects</li>
+                    </ul>
+                </div>
+            </nav>
+        );
+    };
+};
+ 
+export default HomeBanner;
