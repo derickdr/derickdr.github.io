b3398724218a0bcadacdf7f289dc47aea3c7ac56
Derick Del Rosario <derickmitchel@icloud.com>
2020-07-06 14:45:19 -0700

    removed useless constructor

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -2,16 +2,10 @@ import React, {Component} from 'react';
 
 export default class NavigationBar extends Component {
 
-    constructor(props) {
-    super(props)
-    }
-
     render() {
-        const element = (<div>Text from Element</div>)
-        return (<div className="comptext">
-        <h3>First Component</h3>
-            {this.props.displaytext}
-            { element }
-        </div>)
-    }
-}
+        return (
+            <div class="comptext">
+            </div>
+        );
+    };
+};
